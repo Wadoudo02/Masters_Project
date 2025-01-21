f045e3a00f50f02e1e75b43ffdd445804206bdba
Avighna Jha <avighnajha1703@gmail.com>
2025-01-21 15:01:09 +0000

    Completed diphoton plot over categories.

--- SMEFT_classification.py
+++ SMEFT_classification.py
@@ -36,24 +36,6 @@ print(f"--> Remaining rows = {len(ttH_df)}")
 
 #special_features = ["lead_pt_sel", "HT_sel", "cosDeltaPhi_sel" ,"pt-over-mass_sel", "deltaR_sel", "min_delta_R_j_g_sel", "delta_phi_jj_sel", "sublead_pt-over-mass_sel", "delta_eta_gg_sel", "lead_pt-over-mass_sel", "delta_phi_gg_sel"]
 special_features = ["deltaR_sel", "HT_sel", "n_jets_sel", "delta_phi_gg_sel","lead_pt-over-mass_sel"] 
-# EFT_weights = np.asarray(calc_weights(ttH_df, cg=c_g, ctg=c_tg))
-# EFT_weights = (EFT_weights/np.sum(EFT_weights))*10000
-# EFT_labels = np.ones(len(EFT_weights))
-# SM_weights = np.asarray(ttH_df["plot_weight"])
-# SM_weights = (SM_weights/np.sum(SM_weights))*10000
-# SM_labels = np.zeros(len(SM_weights))
-
-
-# comb_df_SM = pd.concat([ttH_df[var] for var in special_features], axis=1)
-# comb_df_SM["weight"] = SM_weights
-
-# comb_df_EFT = comb_df_SM.copy()#pd.concat([ttH_df[var] for var in special_features], axis=1)
-# comb_df_EFT["weight"] = EFT_weights
-
-# #EFT=1, SM=0
-# labels = np.concatenate([EFT_labels, SM_labels])
-# comb_df = pd.concat([comb_df_EFT, comb_df_SM], axis = 0)
-# comb_df["labels"] = labels
 
 comb_df=get_labeled_comb_df(ttH_df, "dup", special_features, c_g, c_tg)
 
@@ -112,7 +94,7 @@ loss_values = []
 val_loss_values = []
 
 # Training loop
-num_epochs = 10
+num_epochs = 100
 
 for epoch in range(num_epochs):
     # Forward pass
@@ -172,10 +154,12 @@ plt.grid()
 plt.show()
 # %%
 #Extract relevant columns from overall df
-cats = [0,0.4, 0.5, 0.6, 0.7,1]
+cats = [0, 0.4, 0.5, 0.6, 0.7,1]
 
 dfs = get_dfs(new_sample_path)
 for proc, df in dfs.items():
+    if proc=="ttH":
+        df=ttH_df
     new_df = pd.concat([df[feature] for feature in special_features]+[df["mass_sel"],df["plot_weight"]], axis=1)
     dfs[proc] = torch.tensor(new_df.to_numpy(), dtype=torch.float32)
 #%%
@@ -184,9 +168,7 @@ dfs_preds = {}
 dfs_cats = {}
 
 for proc, df in dfs.items():
-    print(proc, df)
-    if proc=="ttH":
-        continue
+    #print(proc, df)
     mass,weight = df[:,-2],df[:,-1]
     df = df[:,:-2]
     #new_df = torch.tensor(df,dtype=torch.float32)
@@ -196,25 +178,29 @@ for proc, df in dfs.items():
 
 #%%
 #Split events by category
+with_back=False
 for proc, df in dfs.items():
-    if proc=="ttH":
-        continue
     preds,mass, weights = dfs_preds[proc]
-    dfs_cats[proc]={"events":[], "mass":[],"weights":[]}
+    dfs_cats[proc]={"mass":[],"weights":[]}
     for i in range(1, len(cats)):
         bools = ((cats[i-1]<preds) & (preds<cats[i])).squeeze()
-        dfs_cats[proc]["events"].append(df[bools.numpy()])
+        #print(i, bools.unique())
+        #dfs_cats[proc]["events"].append(df[bools.numpy()])
         dfs_cats[proc]["weights"].append(weights[bools.numpy()])
         dfs_cats[proc]["mass"].append(mass[bools.numpy()])
 
 #Over number of cats
-fig, ax = plt.subplots(ncols=4,figsize=(10, 5))    
-for i in range(1,len(dfs_cats["ggH"])):
+fig, ax = plt.subplots(ncols=len(dfs_cats["ggH"]["mass"]),figsize=(20, 5))    
+for i in range(len(dfs_cats["ggH"]["mass"])):
     for proc, df in dfs_cats.items():
-        print(proc, "cat: ", i)
-        print("events: ", df["events"][i].shape, "Weights: ", df["weights"][i].shape)
+        if with_back and proc=="background":
+            continue
+        #print(proc, "cat: ", i, dfs_cats[proc]["mass"][i].shape)
         ax[i].hist(dfs_cats[proc]["mass"][i],weights=dfs_cats[proc]["weights"][i], bins=50, alpha=0.5, label=proc, )
-
+        ax[i].legend()
+        ax[i].set_title(f"Category {i}")
+        ax[i].set_xlabel("mass (GeV)")
+        ax[i].set_ylabel("Events")
 
 
 
